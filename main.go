24c8bb7c66d7c2193b21f5b0331881f664ba38c5
JaeJinByun <byunjin11@naver.com>
2023-01-09 02:33:51 +0900

    [UPDATE] 소켓 우아한 종료, 유틸 함수 사용

--- main.go
+++ main.go
@@ -4,9 +4,14 @@ import (
 	"context"
 	"flag"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	conf "goEthSub/config"
 	logger "goEthSub/logger"
+	"goEthSub/utils"
 
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
@@ -19,14 +24,15 @@ func main() {
 	flag.Parse()
 	cf := conf.NewConfig(*configFlag)
 	/* 로그 설정 */
-	logger.InitLogger(cf.Log.Fpath)
+	logger.InitLogger(cf)
 
 	/* 1. 이벤트 로그를 구독하기 위해 webSocket 지원 Ethereum 클라이언트에 다이얼을 연결 */
 	client, err := ethclient.Dial(cf.Goerli.ApiKey)
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		log.Print("Transaction Daemon Server Start")
+		logger.Info("Transaction Daemon Server Start")
+		log.Println("Transaction Daemon Server Start")
 	}
 
 	/* 2. 필터 쿼리를 생성 , 이벤트를 구독할 컨트랙트의 주소를 입력 */
@@ -37,6 +43,13 @@ func main() {
 
 	/* 3. 이벤트를 수신하는 방식은 Go 채널을 이용 */
 	logs := make(chan types.Log)
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Daemon...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
 	/* 4. SubscribeFilterLogs 쿼리 옵션과 출력 채널을 받는 클라이언트에서 호출하여 구독 */
 	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
@@ -50,7 +63,12 @@ func main() {
 		case err := <-sub.Err():
 			log.Fatal(err)
 		case vLog := <-logs:
-			log.Printf("%+v", vLog)
+			TransactionLog, _ := utils.Log(vLog).MarshalJSON()
+			logger.Event(string(TransactionLog))
+		case <-ctx.Done():
+			log.Panicln("Timeout of 3 seconds.")
+			client.Close()
 		}
 	}
+
 }
