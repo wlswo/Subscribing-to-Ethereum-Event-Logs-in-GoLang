482feebbb761935edbb12a7d4055f4dd971af450
JaeJinByun <byunjin11@naver.com>
2023-01-08 21:36:16 +0900

    [ADD] 설정파일 추가

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"os"
+
+	"github.com/naoina/toml"
+)
+
+type Config struct {
+	Goerli struct {
+		ApiKey string
+	}
+	Log struct {
+		Fpath string
+	}
+	ContractAddress struct {
+		Ca string
+	}
+}
+
+func NewConfig(fpath string) *Config {
+	c := new(Config)
+
+	if file, err := os.Open(fpath); err != nil {
+		panic(err)
+	} else {
+		defer file.Close()
+		//toml 파일 디코딩
+		if err := toml.NewDecoder(file).Decode(c); err != nil {
+			panic(err)
+		} else {
+			return c
+		}
+	}
+}
