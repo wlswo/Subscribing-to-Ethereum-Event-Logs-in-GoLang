f61cf7d30a9b696beca8ee793cd987aba79aacc7
JaeJinByun <byunjin11@naver.com>
2023-01-13 18:10:04 +0900

    [UPDATE] 감지할 주소 추가

---
+++ models/model.go
@@ -0,0 +1,29 @@
+package models
+
+type Block struct {
+	BlockHash    string        `bson:"blockHash"`
+	BlockNumber  uint64        `bson:"blockNumber"`
+	GasLimit     uint64        `bson:"gasLimit"`
+	GasUsed      uint64        `bson:"gasUsed"`
+	Time         uint64        `bson:"timestamp"`
+	Nonce        uint64        `bson:"nonce"`
+	Transactions []Transaction `bson:"transactions"`
+}
+
+type Transaction struct {
+	TxHash      string `bson:"hash"`
+	From        string `bson:"from"`
+	To          string `bson:"to"` // 컨트랙트의 경우 nil 반환
+	Nonce       uint64 `bson:"nonce"`
+	GasPrice    uint64 `bson:"gasPrice"`
+	GasLimit    uint64 `bson:"gasLimit"`
+	Amount      uint64 `bson:"amount"`
+	BlockHash   string `bson:"blockHash"`
+	BlockNumber uint64 `bson:"blockNumber"`
+}
+
+var Address []string
+
+func init() {
+	Address = append(Address, "0x0924Dfa9cA977e6A956f0399F959c495E54152cb", "0x3764D8e80BB0CbfAA9B4bB6973EdeE0494d2D1eb")
+}
