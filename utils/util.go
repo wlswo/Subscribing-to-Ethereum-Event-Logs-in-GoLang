0a7c2f12620975f25724fe58cc2eec753c8f0639
JaeJinByun <byunjin11@naver.com>
2023-01-13 18:09:22 +0900

    [UPDATE] block바인딩, 트랜잭션 추출 함수 분리

--- utils/util.go
+++ utils/util.go
@@ -2,9 +2,11 @@ package utils
 
 import (
 	"encoding/json"
+	"goEthSub/models"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
 )
 
 type Log struct {
@@ -58,4 +60,54 @@ func (l Log) MarshalJSON() ([]byte, error) {
 	enc.Index = l.Index
 	enc.Removed = l.Removed
 	return json.Marshal(&enc)
+}
+
+func BindingBlock(block *types.Block) (models.Block, error) {
+	var b models.Block
+
+	b.BlockHash = block.Hash().Hex()
+	b.BlockNumber = block.Number().Uint64()
+	b.GasLimit = block.GasLimit()
+	b.GasUsed = block.GasUsed()
+	b.Time = block.Time()
+	b.Nonce = block.Nonce()
+	b.Transactions = make([]models.Transaction, 0)
+	return b, nil
+}
+
+func GetTransactionsFromBlock(txs types.Transactions, b *models.Block, block *types.Block) error {
+
+	if len(txs) > 0 {
+		for _, tx := range txs {
+			msg, err := tx.AsMessage(types.LatestSignerForChainID(tx.ChainId()), block.BaseFee())
+			if err != nil {
+				return err
+			}
+
+			/* 특정 주소에서 발생한 트랜잭션만 감지 */
+			for _, ads := range models.Address {
+				if msg.From().Hex() == ads {
+					// 트랜잭션 구조체 생성
+					t := models.Transaction{
+						TxHash:      tx.Hash().Hex(),
+						To:          "", // 디폴트 값 처리
+						From:        msg.From().Hex(),
+						Nonce:       tx.Nonce(),
+						GasPrice:    tx.GasPrice().Uint64(),
+						GasLimit:    tx.Gas(),
+						Amount:      tx.Value().Uint64(),
+						BlockHash:   block.Hash().Hex(),
+						BlockNumber: block.Number().Uint64(),
+					}
+
+					if tx.To() != nil {
+						t.To = tx.To().Hex()
+					}
+
+					b.Transactions = append(b.Transactions, t)
+				}
+			}
+		}
+	}
+	return nil
 }
