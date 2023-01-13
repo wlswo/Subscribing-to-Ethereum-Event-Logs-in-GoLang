41e91a76f3a1ba70f825eae36bf56cdd08014b73
JaeJinByun <byunjin11@naver.com>
2023-01-13 18:22:56 +0900

    [UPDATE] DB Access 기능 추가

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	conf "goEthSub/config"
 	logger "goEthSub/logger"
+	"goEthSub/models"
 	"goEthSub/utils"
 
 	"github.com/ethereum/go-ethereum"
@@ -26,8 +24,13 @@ func main() {
 	/* 로그 설정 */
 	logger.InitLogger(cf)
 
-	/* 1. 이벤트 로그를 구독하기 위해 webSocket 지원 Ethereum 클라이언트에 다이얼을 연결 */
-	client, err := ethclient.Dial(cf.Goerli.ApiKey)
+	/* model 초기화 */
+	md, err := models.NewModel(cf.DB.Host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := ethclient.Dial(cf.Wemix.Url)
 	if err != nil {
 		log.Fatal(err)
 	} else {
@@ -35,39 +38,60 @@ func main() {
 		log.Println("Transaction Daemon Server Start")
 	}
 
-	/* 2. 필터 쿼리를 생성 , 이벤트를 구독할 컨트랙트의 주소를 입력 */
+	/* 배포한 컨트랙트의 이벤트를 구독할 컨트랙트의 주소를 입력 */
 	contractAddress := common.HexToAddress(cf.ContractAddress.Ca)
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{contractAddress},
 	}
 
-	/* 3. 이벤트를 수신하는 방식은 Go 채널을 이용 */
-	logs := make(chan types.Log)
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("Shutdown Daemon...")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	/* 4. SubscribeFilterLogs 쿼리 옵션과 출력 채널을 받는 클라이언트에서 호출하여 구독 */
-	sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+	/* 블록 감지 */
+	headers := make(chan *types.Header)
+	sub, err := client.SubscribeNewHead(context.Background(), headers)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	/* 5. 새 로그 이벤트를 읽기위해 select문으로 연속 루프를 설정 */
+	/* SubscribeFilterLogs 쿼리 옵션과 출력 채널을 받는 클라이언트에서 호출하여 구독 */
+	logsContract := make(chan types.Log)
+	subContract, err := client.SubscribeFilterLogs(context.Background(), query, logsContract)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* 새 로그 이벤트를 읽기위해 select문으로 연속 루프를 설정 */
 	for {
 		select {
+		case err := <-subContract.Err():
+			log.Fatal(err)
+		case vLog := <-logsContract:
+			TransactionLog, _ := utils.Log(vLog).MarshalJSON()
+			fmt.Println("CA :", string(TransactionLog))
+			logger.Event(string(TransactionLog))
 		case err := <-sub.Err():
 			log.Fatal(err)
-		case vLog := <-logs:
-			TransactionLog, _ := utils.Log(vLog).MarshalJSON()
-			logger.Event(string(TransactionLog))
-		case <-ctx.Done():
-			log.Panicln("Timeout of 3 seconds.")
-			client.Close()
+		case header := <-headers:
+			block, err := client.BlockByNumber(context.Background(), header.Number)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			/* 블록 구조체 생성 */
+			b, err := utils.BindingBlock(block)
+			if err != nil {
+				log.Fatal(err)
+			}
+			/* 트랜잭션 추출 */
+			err = utils.GetTransactionsFromBlock(block.Transactions(), &b, block)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			/* 트랜잭션이 존재할 경우만 DB에 저장 */
+			if len(b.Transactions) > 0 {
+				if err := md.SaveBlock(&b); err != nil {
+					log.Fatal(err)
+				}
+			}
 		}
 	}
 
